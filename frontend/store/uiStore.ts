53e0bb219708dcbdb383fe1f26de0b0951f220a0
Hesyitena <alaikas66@gmail.com>
2026-06-11 22:24:12 +0700

    feat(store): add UI store for sidebar open/close state (mobile)

---
+++ frontend/store/uiStore.ts
@@ -0,0 +1,18 @@
+// SCENTINEL - UI State Zustand Store
+// Manages sidebar open/close state for mobile responsiveness
+import { create } from "zustand";
+
+interface UIState {
+  sidebarOpen: boolean;
+  toggleSidebar: () => void;
+  openSidebar: () => void;
+  closeSidebar: () => void;
+}
+
+export const useUIStore = create<UIState>((set) => ({
+  sidebarOpen: false,
+
+  toggleSidebar: () => set((s) => ({ sidebarOpen: !s.sidebarOpen })),
+  openSidebar: () => set({ sidebarOpen: true }),
+  closeSidebar: () => set({ sidebarOpen: false }),
+}));
